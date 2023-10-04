0d091a57bf110ea97aab7c2058b764081686235f
FitraRamadhan0512 <ramadanfitra123@gmail.com>
2023-10-04 18:47:13 +0700

    first commit

---
+++ deskripsi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Description Book</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+        <img src="sampul.jpg" alt="sampul">
+        <div class="judul">
+            <h3>Fantasy,Drama,Fiction<br></h3>
+            <h1>Klara and The Sun</h1>
+            <h2>By Kaguo Ishiguro</h2>
+            <button type="submit">+ Add to Cart</button>
+            <hr>
+        </div>
+            <p><span class="teks">Summary</span><br><br> ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam cum esse temporibus recusandae facere vel quae veniam. Molestias consectetur, eos recusandae fugit aliquid amet, id ut itaque beatae praesentium unde. Corporis in odit velit! Aut atque, deleniti optio facilis est incidunt ipsum. Amet, natus? Ab obcaecati libero unde? Suscipit neque mollitia illum nostrum blanditiis earum enim natus itaque expedita, provident modi magni minima in tempora voluptatibus consequatur qui veritatis dicta? Tenetur cum accusamus veritatis voluptatibus perferendis itaque tempora iure debitis! In, voluptates! Animi, laudantium placeat eligendi ipsam voluptate ut assumenda. Aliquam nesciunt minus sed error quo facere dolorum! Alias quo, non ullam distinctio fugit eius mollitia. Eum sit nam eaque libero quaerat ullam explicabo quos cupiditate. Consequatur cumque aperiam vero aliquam laboriosam eos quibusdam accusamus expedita veritatis, corporis ratione earum debitis aliquid, accusantium facere voluptatum praesentium. Saepe optio veniam sapiente eum nulla deleniti inventore ratione maiores voluptate! Placeat, labore, id totam, delectus tempora quo necessitatibus quas architecto expedita exercitationem sed quaerat aut repellendus rem officia vero tempore deserunt. Iste sit at quas, delectus autem praesentium ab quisquam modi ea sint pariatur iusto nesciunt, qui repellendus omnis quis quibusdam! Nulla sit suscipit quae reprehenderit culpa quos dolorem corporis omnis eveniet.<br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptatum harum nisi rerum sapiente facilis animi earum minima, iste ut autem tenetur sed voluptatibus veritatis, voluptates dignissimos tempora? Porro excepturi repellendus minima odio iure temporibus nam, corporis inventore ad perferendis optio obcaecati non assumenda harum mollitia nostrum nesciunt quia deleniti modi corrupti, illo cum fuga dolor! Neque quidem dolor culpa provident et alias, corporis quos ratione ipsa eligendi totam odit quo consequuntur ad temporibus blanditiis ex placeat dolorem eos sit officiis, autem nihil dolorum. Exercitationem modi id porro dignissimos optio beatae excepturi fugit, libero incidunt laudantium officia consectetur dicta aliquid quos quisquam. Debitis officiis iste eaque id libero voluptatem nulla ipsa ut obcaecati? Facilis culpa temporibus modi. Quos vitae debitis ullam, temporibus eligendi atque ipsa illum sit mollitia explicabo quibusdam! Facere adipisci sunt, consequuntur obcaecati illum accusantium aliquam natus eaque doloremque esse totam magni voluptates laborum sed ullam delectus aut dolor commodi quam! Distinctio eligendi odit eum quidem temporibus, deserunt ipsa esse! Provident nisi doloribus, nesciunt incidunt nulla, maiores quisquam explicabo voluptatem eveniet cum, perspiciatis reprehenderit sunt! Adipisci voluptate dolores aliquam est quo, unde consectetur sunt quia quae maxime. Corporis quod doloremque eos repellendus error minus esse provident qui hic.</p>
+    </div>
+</body>
+</html>
